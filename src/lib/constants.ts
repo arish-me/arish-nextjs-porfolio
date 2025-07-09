9617b69c7095551d043156275cd693f88ec016f2
Arish <thermic.arish@gmail.com>
2025-07-09 13:17:37 +0530

    SEO Redirect

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,14 +2,14 @@
 
 export const isProduction = process.env.NODE_ENV === 'production'
 
-export const SITE_URL = isProduction ? 'https://www.arishdev.com' : 'http://localhost:3000'
+export const SITE_URL = isProduction ? 'https://www.arishdev.com/' : 'http://localhost:3000'
 
 export const GITHUB_USERNAME = 'arish-me'
 
 export const SITE_NAME = 'Arish';
 export const SITE_TITLE = 'Arish – Full Stack Ruby on Rails & React Developer amd Web Application Specialist';
 export const SITE_DESCRIPTION = 'Experienced Full Stack Developer specializing in Ruby on Rails & React. I build robust web applications with dynamic UIs, efficient backends, and seamless deployment using AWS, Docker, Kubernetes.';
-export const OG_IMAGE_URL = `${SITE_URL}/arishkhan.jpg`
+export const OG_IMAGE_URL = `${SITE_URL}arishkhan.jpg`
 export const SITE_KEYWORDS = [
   'arish-me',
   'Arish developer',
