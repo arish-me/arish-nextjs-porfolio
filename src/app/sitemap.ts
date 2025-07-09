3c3d51b4034621455a558fc591e9451492f6ff3c
Arish <thermic.arish@gmail.com>
2025-07-09 12:09:19 +0530

    Sitemap Update

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,23 +3,23 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.arishdev.com', // Corrected
+      url: 'https://www.arishdev.com/', // Homepage URL should also have a trailing slash if all others do for consistency
       lastModified: new Date(),
     },
     {
-      url: 'https://arishdev.com/about/', // Added trailing slash
+      url: 'https://www.arishdev.com/about/', // Added www
       lastModified: new Date(),
     },
     {
-      url: 'https://arishdev.com/blogs/', // Added trailing slash
+      url: 'https://www.arishdev.com/blogs/', // Added www
       lastModified: new Date(),
     },
     {
-      url: 'https://arishdev.com/projects/', // Added trailing slash
+      url: 'https://www.arishdev.com/projects/', // Added www
       lastModified: new Date(),
     },
     {
-      url: 'https://arishdev.com/contact/', // Added trailing slash
+      url: 'https://www.arishdev.com/contact/', // Added www
       lastModified: new Date(),
     },
   ]
