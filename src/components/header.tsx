b2068a66db5b505890149e8ec23bd9123876f7d8
Arish <thermic.arish@gmail.com>
2025-07-15 17:16:12 +0530

    SEO v 0.1.6

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -72,7 +72,10 @@ export default function Header() {
         >
           <div className="space-y-3">
             <h1 className="font-title bg-gradient-to-b from-black via-black/90 to-black/70 to-90% bg-clip-text text-2xl font-bold leading-9 text-transparent sm:text-4xl sm:leading-[3.5rem] dark:from-white dark:via-white/90 dark:to-white/70">
-              I'm Arish – Full Stack Ruby on Rails & React Developer Web Application Specialist Creating{" "}
+                I'm Arish – Full Stack Ruby on Rails & React Developer
+            </h1>
+            <h2 className="font-title bg-gradient-to-b from-black via-black/90 to-black/70 to-90% bg-clip-text text-2xl font-bold leading-9 text-transparent sm:text-4xl sm:leading-[3.5rem] dark:from-white dark:via-white/90 dark:to-white/70">
+              Web Application Specialist Creating{" "}
               <div className="inline-grid h-9 overflow-hidden sm:h-14">
                 <div ref={scope}>
                   {TEXTS.map(({ text, className }, i) => (
@@ -83,7 +86,8 @@ export default function Header() {
                 </div>
               </div>{" "}
               web applications
-            </h1>
+            </h2>
+            
           </div>
         </motion.div>
 
