ce57043031c29f73e2931b9942a2b0a7320e1cd0
Arish <thermic.arish@gmail.com>
2025-07-15 15:17:28 +0530

    Fix Typo

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,7 +7,7 @@ export const SITE_URL = isProduction ? 'https://www.arishdev.com/' : 'http://loc
 export const GITHUB_USERNAME = 'arish-me'
 
 export const SITE_NAME = 'Arish';
-export const SITE_TITLE = 'Arish – Full Stack Ruby on Rails & React Developer amd Web Application Specialist';
+export const SITE_TITLE = 'Arish – Full Stack Ruby on Rails & React Developer and Web Application Specialist';
 export const SITE_DESCRIPTION = 'Experienced Full Stack Developer specializing in Ruby on Rails & React. I build robust web applications with dynamic UIs, efficient backends, and seamless deployment using AWS, Docker, Kubernetes.';
 export const OG_IMAGE_URL = `${SITE_URL}arishkhan.jpg`
 export const SITE_KEYWORDS = [
