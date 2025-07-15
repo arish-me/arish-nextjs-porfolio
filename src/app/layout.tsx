c16013718e92234012569c3091d493ba1c8b9658
Arish <thermic.arish@gmail.com>
2025-07-15 16:26:26 +0530

    SEO v 0.1.5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata = generateMetadata(undefined, undefined, undefined, undefined, '/');
+export const metadata = generateMetadata(undefined, undefined, undefined, undefined, '');
 
 export default function RootLayout({
   children,
